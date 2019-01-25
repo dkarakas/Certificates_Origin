2b0cb3ff56c917c6f96f19e3a6948b8d658882ca
Dimcho Karakashev <dzkaraka@scspc709.cs.uwaterloo.ca>
2019-01-25 14:26:51 -0500

    temp state while implementing uploading of contracts. Issues when commands are executed with no time in between

--- src/client_docker.py
+++ src/client_docker.py
@@ -1,8 +1,13 @@
 import docker
 import subprocess
 import os
+import time
+from src.data_xml_to_json  import xls_parser
 
-class fabric_docker:
+
+class DockerFabric:
+    CHANNEL_NAME = "mychannel"
+    ORG = "agiletech.vn"
 
     def __init__(self, *args):
         if len(args) != 1:
@@ -36,7 +41,7 @@ class fabric_docker:
         print(out.decode('utf8', 'ignore') + " " + err.decode('utf8', 'ignore'))
 
         # Generating artificats
-        command = "yarn genConfig -domain agiletech.vn -Kafka 3 -Orderer 2 -Zookeeper 3 -Orgs 2 -Peer 1 -Tag :x86_64-1.1.0-preview"
+        command = "yarn genConfig -domain agiletech.vn -Kafka 3 -Orderer 2 -Zookeeper 3 -Orgs 2 -Peer 1 -Tag :latest"
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = process.communicate()
         print(out.decode('utf8', 'ignore') + " " + err.decode('utf8', 'ignore'))
@@ -46,43 +51,118 @@ class fabric_docker:
         out, err = process.communicate()
         print(out.decode('utf8', 'ignore') + " " + err.decode('utf8', 'ignore'))
 
-        #starting network
+        # starting network
         command = "yarn start"
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = process.communicate()
         print(out.decode('utf8', 'ignore') + " " + err.decode('utf8', 'ignore'))
 
-    def create_channel(self):
-        cli_container = None
-        client = docker.from_env()
-
-        #finding the id of the client container
-        for container in client.containers.list(all):
-            if "cli_cli" in container.name:
-                cli_container = container
-        print(cli_container)
-        ret = cli_container.exec_run("bash CHANNEL_NAME=mychannel")
-        print(ret.output.decode('utf8', 'ignore'))
-        ret = cli_container.exec_run("bash export CHANNEL_NAME")
-        print(ret.output.decode('utf8', 'ignore'))
-        ret = cli_container.exec_run("sh export ORG=agiletech.vn")
-        print(ret.output.decode('utf8', 'ignore'))
-        ret = cli_container.exec_run("peer channel create -o orderer0.${ORG}:7050 -c $CHANNEL_NAME "
-                                     "-f ./channel-artifacts/channel.tx --tls true --cafile"
-                                     " /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations"
-                                     "/$ORG/orderers/orderer0.${ORG}/msp/tlscacerts/tlsca.${ORG}-cert.pem")
-        print(ret.output.decode('utf8', 'ignore'))
-
-
-    def close(self):
-        #containers
+        # getting all containers
         command = "docker ps -aq"
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = process.communicate()
         print(out.decode('utf8', 'ignore') + " " + err.decode('utf8', 'ignore'))
         list_cont = out.decode('utf8', 'ignore').split()
 
-        # print(process.stdout)
+        # for some reason even if docker stop is used containers restart. This fixes the issue
+        print("updating the containers so we can stop them")
+        for container in list_cont:
+            command = "docker update --restart=unless-stopped " + str(container)
+            process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = process.communicate()
+            print(out.decode('utf8', 'ignore') + " " + err.decode('utf8', 'ignore'))
+
+
+        sleep = 30
+        while sleep  >0:
+            print("Waiting for all containers to start: " + str(sleep) + " seconds left")
+            time.sleep(1)
+            sleep -= 1
+
+    def find_cli(self):
+        cli_container = None
+        client = docker.from_env()
+
+        # finding the id of the client container
+        for container in client.containers.list(all):
+            if "cli_cli" in container.name:
+                cli_container = container
+        return cli_container
+
+    def create_channel(self):
+        cli_container = self.find_cli()
+
+        # creating a channel TODO: fix the variables!!!
+        exict_code, output = cli_container.exec_run("peer channel create -o orderer0." + self.ORG + ":7050 -c " +
+                                                    self.CHANNEL_NAME +" -f ./channel-artifacts/channel.tx --tls "
+                                                    "true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/"
+                                                                       "crypto/ordererOrganizations/" +
+                                                    self.ORG + "/orderers/orderer0." + self.ORG + "/msp/tlscacerts/"
+                                                    "tlsca." + self.ORG + "-cert.pem",
+                                                    stream=True)
+        for line in output:
+            print("create_channel: " + str(line))
+
+    def join_channel(self):
+        cli_container = self.find_cli()
+
+        exict_code, output = cli_container.exec_run("peer channel join -b " + self.CHANNEL_NAME + ".block", stream=True)
+        for line in output:
+            print(line)
+
+        exict_code, output = cli_container.exec_run("peer channel list", stream=True)
+        for line in output:
+            print("join_channel: " + str(line))
+
+    def install_chaincode(self):
+        cli_container = self.find_cli()
+
+        exict_code, output = cli_container.exec_run(" peer chaincode install -n mycc -v 1.0 -p github.com/hyperledger"
+                                                    "/fabric/examples/chaincode/go/sacc", stream=True)
+        for line in output:
+            print(str(line))
+
+        exict_code, output = cli_container.exec_run(
+            "peer chaincode instantiate -o orderer0." + self.ORG + ":7050 --tls true"
+            " --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto"
+            "/ordererOrganizations/" + self.ORG + "/orderers/orderer0." + self.ORG +
+            "/msp/tlscacerts/tlsca." + self.ORG + "-cert.pem -C " + self.CHANNEL_NAME +
+            " -n mycc -v 1.0 -c '{\"Args\":[\"a\", \"100\"]}'", stream=True)
+        for line in output:
+            print("install_chaincode: " + str(line))
+
+    def query_entry(self, userID):
+        cli_container = self.find_cli()
+        exict_code, output = cli_container.exec_run("peer chaincode query -C " + self.CHANNEL_NAME + " -n mycc "
+                                                    "-c '{\"Args\":[\"query\",\"" + userID + "\"]}'", stream=True)
+        for line in output:
+            print("query_entry: " + str(line))
+
+    def set_entry(self, userID , certificate):
+        cli_container = self.find_cli()
+        exict_code, output = cli_container.exec_run(
+            "peer chaincode invoke -o orderer1." + self.ORG + ":7050 --tls true --cafile /opt/gopath/src/github.com/"
+            "hyperledger/fabric/peer/crypto/ordererOrganizations/" + self.ORG + "/orderers/orderer1." + self.ORG + "/msp"
+            "/tlscacerts/tlsca." + self.ORG + "-cert.pem -C " + self.CHANNEL_NAME + " -n mycc -c "
+            "'{\"Args\":[\"set\",\"" + userID + "\",\"" + certificate + "\"]}'", stream=True)
+        for line in output:
+            print("set_entry: " + str(line))
+
+    def close(self):
+        # containers
+        command = "docker ps -aq"
+        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = process.communicate()
+        print(out.decode('utf8', 'ignore') + " " + err.decode('utf8', 'ignore'))
+        list_cont = out.decode('utf8', 'ignore').split()
+
+        # for some reason even if docker stop is used containers restart. This fixes the issue
+        for container in list_cont:
+            command = "docker update --restart=unless-stopped " + str(container)
+            process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = process.communicate()
+            print(out.decode('utf8', 'ignore') + " " + err.decode('utf8', 'ignore'))
+
         # stopping containers
         print("Stopping containers")
         for container in list_cont:
@@ -99,7 +179,7 @@ class fabric_docker:
             out, err = process.communicate()
             print(out.decode('utf8', 'ignore') + " " + err.decode('utf8', 'ignore'))
 
-        #containers
+        # containers
         command = "docker ps -aq"
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = process.communicate()
@@ -108,8 +188,25 @@ class fabric_docker:
 
 if __name__ == "__main__":
     dir = os.path.abspath(os.path.join(__file__, os.path.pardir, os.path.pardir, os.path.pardir))
+    # print(dir)
+    # dir_path = dir + "/Certificates_Origin/XLS_data/Xantrex_Inverter_Data.zip.gpg"
+    # parser = xls_parser(dir_path)
+    # parser.close()
+    # xls_files = parser.decrypt_and_unzip()
+    # list_of_certificates = parser.extract_from_xls(xls_files)
+    # parser.create_json(list_of_certificates)
+    # print(list_of_certificates)
+
+
     dir = dir + "/hyperledger-fabric-swarm"
-    docker_controller = fabric_docker(dir)
+    docker_controller = DockerFabric(dir)
     # docker_controller.start()
-    docker_controller.create_channel()
+    # docker_controller.create_channel()
+    # docker_controller.join_channel()
+    # docker_controller.install_chaincode()
+    #
+    # ## For test
+    # docker_controller.query_entry("a")
+    # docker_controller.set_entry("a", "10")
+    docker_controller.query_entry("a")
     # docker_controller.close()
